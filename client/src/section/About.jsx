ed2ddfb9e61cd7b100a8989e17750f0e0d97dad2
satoshi-sh <satoss1108@gmail.com>
2023-09-02 14:12:04 -0500

    Added an image to the about section

--- client/src/section/About.jsx
+++ client/src/section/About.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about" className="p-36">
-      <p className="h-64">from about</p>
+    <div
+      id="about"
+      className="flex p-24 justify-center pt-15 md:p-24 lg:p-32 gap-5 flex-wrap"
+    >
+      <div className="min-w-[150px]">
+        <img
+          src="/images/satoshi.webp"
+          alt="My Image"
+          className="rounded-full"
+        />
+      </div>
+      <div>
+        <h1 className="text-[3rem] font-thin">
+          Hi, I'm <span className="font-bold">Satoshi Shirosaka</span>
+        </h1>
+        <p className="max-w-[800px] mt-10 text-[1.2rem]">
+          I'm a <span className="font-bold">Full-Stack developer</span> with
+          economics education background and a passion for building web
+          applications. I bring a unique blend of technical expertise and
+          analytical thinking to every project.
+        </p>
+      </div>
     </div>
   );
 };
