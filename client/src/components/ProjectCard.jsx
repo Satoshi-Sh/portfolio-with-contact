0cb21485ccba388cf0d3253f1c0082051b49ea59
satoshi-sh <satoss1108@gmail.com>
2023-09-02 17:09:03 -0500

    Added technologies sections

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -12,6 +12,15 @@ const ProjectCard = ({ project }) => {
         <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {project.title}
         </h5>
+        <div className="p-2">
+          {project.technologies.map((tech, index) => {
+            return (
+              <span key={index} className="p-1 text-gray-400">
+                #{tech}
+              </span>
+            );
+          })}
+        </div>
         <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
           {project.description}
         </p>
--- client/src/section/Projects.jsx
+++ client/src/section/Projects.jsx
@@ -13,13 +13,7 @@ const projects = [
   },
   {
     title: "Team Todo",
-    technologies: [
-      "React.js",
-      "Express.js",
-      "MongoDB",
-      "WebSocket",
-      "Tailwind CSS",
-    ],
+    technologies: ["React.js", "Express.js", "MongoDB", "WebSocket"],
     image: "/images/team-todo-demo.gif",
     description:
       "This application allows users to create projects and other members take todo tasks. It features real-time updates using websockets to show the status of projects as well as a chat function for communication among team members. This enables seamless collaboration, task assignment, progress tracking, and communication within the application.",
@@ -27,7 +21,7 @@ const projects = [
   },
   {
     title: "Web Dev Path",
-    technogoies: ["Next.js"],
+    technologies: ["Next.js"],
     image: "/images/web-dev-path.jpg",
     description:
       "I'm actively contributing to the open-source initiative project 'Web Dev Path,' where I collaborate on the development and maintenance of a Next.js web application in a simulated professional environment. I'm proud of my contributions, which include integrating Google Analytics, fixing mobile views, reviewing codes and so on.",
