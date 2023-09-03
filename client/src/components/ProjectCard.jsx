7141e4a7858a8f9f8211ad4852ddb3f57bfbaf63
satoshi-sh <satoss1108@gmail.com>
2023-09-02 21:32:55 -0500

    Made a flex box for technologies section

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ project }) => {
         <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {project.title}
         </h5>
-        <div className="p-2">
+        <div className="p-2 flex flex-wrap">
           {project.technologies.map((tech, index) => {
             return (
               <span key={index} className="p-1 text-gray-400">
