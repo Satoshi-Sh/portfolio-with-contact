d5764dba569575be56fc69b63ee6e176603320aa
satoshi-sh <satoss1108@gmail.com>
2023-09-04 12:34:12 -0500

    Added z-index to the navigation bar

--- client/src/section/NavigationBar.jsx
+++ client/src/section/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <nav className="bg-black text-white text-sm md:text-base lg:text-lg xl:text-xl  p-6 fixed w-full flex-wrap top-0">
+    <nav className="bg-black text-white text-sm md:text-base lg:text-lg xl:text-xl  p-6 fixed w-full flex-wrap top-0 z-10">
       <ul className="flex justify-center font-bold gap-10 md:gap-20 lg:gap-20 ">
         <li className="hover:text-green-200 hover:underline decoration-wavy decoration-cyan-400">
           <Link to="/#about">About</Link>
